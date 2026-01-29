b8e7264c8d8348051d8d3370485fbd3b20a81211
sajal <bajajsajal3369@gmail.com>
2026-01-29 17:11:49 +0530

    chnage in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,19 @@ import Footer from "../components/Footer";
 import CategoryBar from "../components/CategoryBar";
 import Carousel from "../components/Carousel";
 import CustomerReviews from "../components/CustomerReviews";
+import { Helmet } from "react-helmet-async";
 import OurPlumbers from "../components/OurPlumbers";
 
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>PVC Pipes in Bareilly | Abhiyaan Sanitary</title>
+        <meta
+          name="description"
+          content="Best PVC & CPVC pipes at best price"
+        />
+      </Helmet>
       <Navbar />
       <CategoryBar />
       <Carousel />
