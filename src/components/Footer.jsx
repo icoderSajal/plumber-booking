d957f815fb4b339ec3dd97487fc39b7521fc8717
Sajal Kumar Bajaj <94426360+icoderSajal@users.noreply.github.com>
2026-04-26 17:35:18 +0530

    change in shop timming

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -95,12 +95,12 @@ import MapSection from "./MapSection";
 
 const Footer = () => {
   const hours = [
-    { id: 1, day: "Monday", time: "9:00 AM - 9:00 PM" },
-    { id: 2, day: "Tuesday", time: "9:00 AM - 9:00 PM" },
-    { id: 3, day: "Wednesday", time: "9:00 AM - 9:00 PM" },
-    { id: 4, day: "Thursday", time: "9:00 AM - 9:00 PM" },
-    { id: 5, day: "Friday", time: "9:00 AM - 9:00 PM" },
-    { id: 6, day: "Saturday", time: "9:00 AM - 9:00 PM" },
+    { id: 1, day: "Monday", time: "8:00 AM - 8:00 PM" },
+    { id: 2, day: "Tuesday", time: "8:00 AM - 8:00 PM" },
+    { id: 3, day: "Wednesday", time: "8:00 AM - 8:00 PM" },
+    { id: 4, day: "Thursday", time: "8:00 AM - 8:00 PM" },
+    { id: 5, day: "Friday", time: "8:00 AM - 8:00 PM" },
+    { id: 6, day: "Saturday", time: "8:00 AM - 8:00 PM" },
   ];
 
   return (
