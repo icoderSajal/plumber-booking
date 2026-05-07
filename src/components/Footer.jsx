10fce6fc530416aca9343ba2a767b73ae9c6964d
sajal <bajajsajal3369@gmail.com>
2026-05-07 13:53:03 +0530

    update email id

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -76,7 +76,7 @@ const Footer = () => {
 
           <div className="flex items-center mb-2 text-gray-300">
             <MdEmail className="mr-2" />
-            <span>abhiyaan@gmail.com</span>
+            <span>abhiyaansanitary@gmail.com</span>
           </div>
 
           {/* Address */}
